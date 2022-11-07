033220722f7528a141ff7a426fd42f88c3eb10d7
Lapratomo24 <lapratomo24@yahoo.com>
2022-11-07 21:15:52 +0000

    create profile serializer

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from profiles import views
 
 
-urlpatters = [
-    path('profiles/', views.ProfileList.as_view())
+urlpatterns = [
+    path('profiles/', views.ProfileList.as_view()),
 ]
