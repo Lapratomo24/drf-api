978d1b2d87efbd804193e90e5c14554a7a09a26a
Lapratomo24 <lapratomo24@yahoo.com>
2022-11-13 17:19:07 +0000

    add extra filter features to posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
 # from django.http import Http404
 from rest_framework import permissions, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 # from rest_framework.response import Response
 # from rest_framework.views import APIView
 from .models import Post
@@ -15,7 +16,20 @@ class PostList(generics.ListAPIView):
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
         ).order_by('-created_at')
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+        ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title'
+    ]
     ordering_fields = ['likes_count', 'comments_count', 'likes__created_at']
 
     def perform_create(self, serializer):
