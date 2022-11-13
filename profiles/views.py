7c06eb8d818ef80d4a539fa87e134e88cb47d70d
Lapratomo24 <lapratomo24@yahoo.com>
2022-11-13 15:52:42 +0000

    add extra fields and filter ordering

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Count
 # from django.http import Http404
-from rest_framework import generics
+from rest_framework import generics, filters
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
 from .models import Profile
@@ -8,13 +9,31 @@ from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at'
+        'owner__followed__created_at'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
 
