e0f45c2313cc4f9cccaa09ecd6091a37f72f5951
Lapratomo24 <lapratomo24@yahoo.com>
2022-11-14 22:07:11 +0000

    retry setting up deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://react-project-5.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'react-project-5.herokuapp.com']
 
 
 # Application definition
